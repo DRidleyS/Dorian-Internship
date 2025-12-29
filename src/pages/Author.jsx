69ac557ffca322164dd757c72f0f013df77591e2
DRidleyS <dorianridley@gmail.com>
2025-12-29 11:36:01 -0800

    made author page dynamic with follow button functionality and refactored authorItems to use props for api from author page

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const Author = () => {
+  const { authorId } = useParams();
+  const [authorData, setAuthorData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    const fetchAuthorData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+        );
+        setAuthorData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching author data:", error);
+        setLoading(false);
+      }
+    };
+    fetchAuthorData();
+  }, [authorId]);
+
+  const handleFollowToggle = () => {
+    setIsFollowing(!isFollowing);
+  };
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +51,20 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img
+                        src={authorData?.authorImage || AuthorImage}
+                        alt=""
+                      />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {authorData?.authorName || "Loading..."}
+                          <span className="profile_username">
+                            @{authorData?.tag || ""}
+                          </span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {authorData?.address || ""}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,10 +75,13 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_follower">
+                        {(authorData?.followers || 0) + (isFollowing ? 1 : 0)}{" "}
+                        followers
+                      </div>
+                      <button onClick={handleFollowToggle} className="btn-main">
+                        {isFollowing ? "Unfollow" : "Follow"}
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -55,7 +89,10 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems
+                    nftCollection={authorData?.nftCollection || []}
+                    loading={loading}
+                  />
                 </div>
               </div>
             </div>
